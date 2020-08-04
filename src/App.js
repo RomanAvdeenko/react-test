b4ec647231508850b707f72fa1d8869ca34e3902
RomanAvdeenko <yes@hl.ua>
2020-08-04 09:47:09 +0300

    202020804

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    console.log('render... ', this.props.testStore);
+    return (
+      <div>
+        <input type="text" className="trackInput" />
+        <button className="addTrack">Add track</button>
+        <ul className="list">
+          {this.props.testStore.map((e, i) =>
+            <li key={i}>{e}</li>
+          )}
+        </ul>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default connect(
+  state => ({ // map state to props
+    testStore: state
+  }),
+  dispatch => ({})
+)(App);
